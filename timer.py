956b0e2a7410e631fc52d348c8b7822c6ce7f945
tangxl <1051742287@qq.com>
2023-11-19 00:07:18 +0800

    改了快捷键 增加小花机制20秒时语音

--- timer.py
+++ timer.py
@@ -66,19 +66,21 @@ class CountdownTimer:
             mins, secs = divmod(self.remaining_seconds, 60)
             timeformat = '{:02d}:{:02d}'.format(mins, secs)
             self.remaining_time.set(f"{self.timer_name}: {timeformat}")
-
+            
+            if self.remaining_seconds == 20 and self.initial_seconds == 75 :
+                    tts_thread = threading.Thread(target=self.speak_timer_xiaohua)
+                    tts_thread.start()
+                    
             if self.remaining_seconds <= 15:
-                self.time_label.config(fg="red")  # Change font color to red
-
+                self.time_label.config(fg="red")  # 改成红色
                 if self.remaining_seconds == 10:
                     tts_thread = threading.Thread(target=self.speak_timer_name)
                     tts_thread.start()
-
             else:
-                self.time_label.config(fg="black")  # Restore font color to black
+                self.time_label.config(fg="black")  # 字体黑色
 
             self.remaining_seconds -= 1
-            self.timer_id = self.root.after(1000, self.update_clock)  # Update the timer
+            self.timer_id = self.root.after(1000, self.update_clock)  # 更新计时器
 
         elif not self.is_running:
             self.is_running = False
@@ -91,6 +93,12 @@ class CountdownTimer:
         # Initialize TTS engine
         engine = pyttsx3.init()
         engine.say(self.timer_name)
+        engine.runAndWait()
+    
+    def speak_timer_xiaohua(self):
+        # Initialize TTS engine
+        engine = pyttsx3.init()
+        engine.say("二十秒后"+self.timer_name)
         engine.runAndWait()
 
 
@@ -125,19 +133,27 @@ def on_f7_press(event):
 def on_f8_press(event):
     timer7.reset_countdown()
     timer7.start_countdown()
+    
+def on_test_press(event):
+    if timer7.is_running == False:
+        timer7.is_running = True
+        timer7.update_clock()
+    elif timer7.is_running == True:
+        timer7.is_running = False
+    
 
 if __name__ == '__main__':
     root = tk.Tk()
     #root.overrideredirect(True)
     root.attributes('-topmost', 1)  # 窗口置于顶层
-    root.title('27-3计时器v1.1 作者联系方式1051742287')
+    root.title('27-3计时器v1.2 作者圈名：七号')
     #全部烧掉时禁锢小花都可以按了，最开始可以按镰刀，4分钟+冰冻延时差不多就好，第一次禁锢按蛋壳，第二次地刺后30秒回修地板，会因为控被延迟
     #修地板从持续17秒左右，结束按来得及
     #控的久可能先地刺再蛋壳
     timer0 = CountdownTimer(root, "地刺", 158, 0, 0)
     timer1 = CountdownTimer(root, "冰冻", 90, 0, 1)
-    timer2 = CountdownTimer(root, "禁锢", 80, 0, 2)
-    timer3 = CountdownTimer(root, "小花", 75, 0, 3)
+    timer2 = CountdownTimer(root, "小花", 75, 0, 2)
+    timer3 = CountdownTimer(root, "禁锢", 80, 0, 3)
     timer4 = CountdownTimer(root, "镰刀", 240, 0, 4)
     timer5 = CountdownTimer(root, "蛋壳", 240, 0, 5)
     timer6 = CountdownTimer(root, "修地板", 300, 0, 6)
@@ -147,12 +163,17 @@ if __name__ == '__main__':
     ]
     keyboard.on_press_key('F1', on_f1_press)
     keyboard.on_press_key('F2', on_f2_press)
+    '''
     keyboard.on_press_key('F3', on_f3_press)
     keyboard.on_press_key('F4', on_f4_press)
-    keyboard.on_press_key('F5', on_f5_press)
-    keyboard.on_press_key('F6', on_f6_press)
-    keyboard.on_press_key('F7', on_f7_press)
-    keyboard.on_press_key('F8', on_f8_press)
+    '''
+    keyboard.on_press_key(79, on_f3_press)
+    keyboard.on_press_key(80, on_f4_press)
+    keyboard.on_press_key(71, on_f5_press)
+    keyboard.on_press_key(72, on_f6_press)
+    keyboard.on_press_key(73, on_f7_press)
+    keyboard.on_press_key(78, on_f8_press)
+    #keyboard.on_press_key('F9', on_test_press)
 
 
     #keyboard.add_hotkey('alt+1', on_f2_press)
